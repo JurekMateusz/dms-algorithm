53512977f8609c9952d6d48e2d212152b8b5b091
Mateusz Jurek <mateuszjurek98@outlook.com>
2021-10-24 16:01:46 +0200

    Create map with values.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,29 +1,80 @@
-$(document).ready(function() {
-  var max_fields      = 999; //maximum input boxes allowed
-  var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
-  var add_button      = $(".add_field_button"); //Add button ID
-  var send_button     = $(".js-button");
+let mapOfTasksJson = new Map();
 
-  var x = 1; //initlal text box count
-  var i = 0;
+$(document).ready(function () {
+  var max_fields = 20; //maximum input boxes allowed
+  var wrapper = $(".input_fields_wrap"); //Fields wrapper
+  var add_button = $(".add_field_button"); //Add button ID
 
-  $(send_button).click(function(event){
+  var send_button = $(".js-button");
+  var x = 2; //initlal text box count
+
+  var taskNumber = 1;
+  $(send_button).click(function (event) {
     event.preventDefault();   //block send function in button form
+
   })
-
-  $(add_button).click(function(e){ //on add input button click
+  $(add_button).click(function (e) { //on add input button click
     e.preventDefault();
-    console.log("aaa");
-    if(x < max_fields){ //max input box allowed
-      x++; //text box increment
-      i++;
-      $(wrapper).append('<p><div class="section__new"><input class="form__input" value="T'+i+'"><input class="form__input" placeholder="P"><input class="form__input" placeholder="C"><input class="form__input" placeholder="D"><div class="input-group-append"><button class="remove_field removeButton" type="button">Remove</button></div></div></p>'); //add input box
+    if (x < max_fields) {
+      //while(existNumberText){
+      //  x++/taskNumber++
+      // }
+      var period = $("#period" + taskNumber).val();
+      var completion = $("#completion" + taskNumber).val();
+      var deadline = $("#deadline" + taskNumber).val();
+      if (period.length === 0 || completion.length === 0 || deadline.length
+          === 0) {
+        return;
+      }
+      mapOfTasksJson.set(taskNumber, {
+        'taskName': "T"+taskNumber,
+        'period': period,
+        'completionTime': completion,
+        'deadline': deadline
+      });
 
+      x++;
+      taskNumber++;
+
+      $(wrapper).append(
+          '<p><div class="section' + taskNumber + '">'
+          + '<input class="form__input" value="T'+taskNumber+'">'
+          + '<input id="period'+ taskNumber+'" class="form__input" placeholder="P">'
+          + '<input id="completion'+ taskNumber+'" class="form__input" placeholder="C">'
+          + '<input id="deadline'+ taskNumber+'" class="form__input" placeholder="D">'
+          + '<div class="input-group-append">'
+          + '<button class="remove_field removeButton" type="button">Remove</button>'
+          + '</div>'
+          + '</div></p>'); //add input box
     }
   });
-  $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-    e.preventDefault(); $(this).parent('div').parent('div').remove(); i--;x--;
+  $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+    e.preventDefault();
+    $(this).parent('div').parent('div').remove();
+    taskNumber--;
+    x--;
   })
+
 });
 
 let buttonSend = document.querySelector(".js-button");
+const getResult = async () => {
+  document.getElementById('show-results').style.display = 'none';
+
+  jsonStr = JSON.stringify(mapToArray(mapOfTasksJson));
+  const response = await fetch('http://localhost:8080/dms/', {
+    method: 'POST',
+    body: jsonStr,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json();
+  showChart(myJson);
+}
+
+function mapToArray(map) {
+  let array = [];
+  map.forEach(x => array.push(x));
+  return array;
+}
